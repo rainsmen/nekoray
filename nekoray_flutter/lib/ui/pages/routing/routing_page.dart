dc0c4089cae5d3dea49dd6b6146a02d1879e927d
rainsmen <rainsmen@users.noreply.github.com>
2026-08-23 07:53:20 +0800

    flutter: fix routing_page import paths

--- nekoray_flutter/lib/ui/pages/routing/routing_page.dart
+++ nekoray_flutter/lib/ui/pages/routing/routing_page.dart
@@ -8,8 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../core/models/profile.dart';
-import '../../core/state/providers.dart';
+import '../../../core/models/profile.dart';
+import '../../../core/state/providers.dart';
 
 /// Routing config (loaded from routing_default.json).
 final routingConfigProvider =
